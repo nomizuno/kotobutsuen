fa0908a7bfb551c43506ac01fbe9dcd8a4a07c03
mizuno <yuma.m0610@gmail.com>
2018-03-18 20:53:44 +0900

    ユーザー名でログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,38 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validates :name,
+            presence: true,
+            uniqueness: {case_sensitive: false }
+  validates_format_of :name, with: /^[a-zA-Z0-9_¥.]^/, multiline: true
+  validate :validate_name
+
+  attr_accessor :login
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.name || self.email
+  end
+
+  def validate_name
+    errors.add(:name, :invalid) if User.where(email: name).exists?
+  end
+
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    conditions[:email].downcase! if conditions[:email]
+    login = conditions.delete(:login)
+
+    where(conditions.to_hash).where(
+      ["lower(name) = :value OR lower(email) = :value",
+      {value: login.downcase}]
+    ).first
+
+  end
+
 end
