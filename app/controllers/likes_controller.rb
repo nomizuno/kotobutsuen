dbab5025f3496a2e5c0f844e5016d6c48a0a3eba
mizuno <yuma.m0610@gmail.com>
2018-03-19 01:01:01 +0900

    likesモデル作成

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,30 @@
+class LikesController < ApplicationController
+
+
+	def create
+		@like = Like.new(
+			user_id: current_user.id,
+			comment_id: params[:comment_id]
+			)
+		@like.save
+		@comment = Comment.find_by(id: params[:comment_id])
+		@comment.likes_count = Like.where(comment_id: params[:comment_id]).count
+		@comment.save
+		@post = Post.find_by(id: @comment.post_id)
+		redirect_to("/posts/#{@post.id}")
+	end
+
+	def destroy
+		@like = Like.find_by(
+			user_id: current_user.id,
+			comment_id: params[:comment_id]
+			)
+		@like.destroy
+		@comment = Comment.find_by(id: params[:comment_id])
+		@comment.likes_count = Like.where(comment_id: params[:comment_id]).count
+		@comment.save
+		@post = Post.find_by(id: @comment.post_id)
+		redirect_to("/posts/#{@post.id}")
+	end
+
+end
