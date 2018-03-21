44945d304c3c1a49e95439c24e860fe7cfa767e2
mizuno <yuma.m0610@gmail.com>
2018-03-21 19:16:02 +0900

    管理

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,6 +6,12 @@ ActiveAdmin.setup do |config|
   #
   config.site_title = "Kotobutsuen"
 
+  config.clear_stylesheets!
+  config.register_stylesheet 'admin/active_admin.css'
+
+  config.clear_javascripts!
+  config.register_javascript 'admin/active_admin.js'
+
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
