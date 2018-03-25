896ee3718a0fe888696c98bf645f5a689da249ab
mizuno <yuma.m0610@gmail.com>
2018-03-25 23:26:38 +0900

    devise

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Kotobutsuen
     config.load_defaults 5.1
     config.read_encrypted_secrets = true
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
