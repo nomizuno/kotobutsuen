64ec66533c385c26b03c641de9cae6187c90883b
mizuno <yuma.m0610@gmail.com>
2018-03-25 23:44:37 +0900

    devise4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   config.action_mailer.smtp_settings = {
   :enable_starttls_auto => true,
   :address => "smtp.gmail.com",
