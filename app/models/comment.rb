bee61d3838fd4aa0588dab0b9c00638375eef05f
mizuno <yuma.m0610@gmail.com>
2018-03-31 18:40:22 +0900

    バリデーション

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  validates :content, {presence: true, length: {maximum: 31}, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }}
+  validates :user_id, {presence: true}
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :content, {presence: true, length: {maximum: 10},uniqueness: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }}
+  validates :user_id, {presence: true}
+  validates :reading, {presence: true,length: {maximum: 20},format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }}
+
 
   def self.search(search) #self.でクラスメソッドとしている
       if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
