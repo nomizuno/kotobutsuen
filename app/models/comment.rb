bfa39b46ceefdcfe4551dd1997f7bbc7c2c13c8c
mizuno <yuma.m0610@gmail.com>
2018-04-06 01:02:12 +0900

    vali

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
-  validates :content, {presence: true, length: {maximum: 31}, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }}
+  # validates :content, {presence: true, length: {maximum: 31}, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }}
+  validates :content, {presence: true, length: {maximum: 31}}
   validates :user_id, {presence: true}
 
 end
