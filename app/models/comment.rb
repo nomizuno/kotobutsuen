4e4afcea39f5a5ef235684aba369a20897332e65
mizuno <yuma.m0610@gmail.com>
2018-04-08 16:59:41 +0900

    ことば投稿後のページ遷移の変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
 
-  validates :content, {presence: true, length: {maximum: 31}, exclusion: { in: %w(www us ca jp) }}
+  validates :content, {presence: true, length: {maximum: 31}, exclusion: { in: %w(www us ca jp http) }}
   validates :content, {presence: true, length: {maximum: 31}}
 
   validates :user_id, {presence: true}
