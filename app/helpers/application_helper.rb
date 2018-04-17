cde1dc60d68c04544abdf18c0060f224c0c17942
mizuno <yuma.m0610@gmail.com>
2018-04-17 23:28:30 +0900

    meta-tags設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,32 @@ module ApplicationHelper
 
   APP_NAME = 'kotobutsuen'
 
+  def default_meta_tags
+   {
+     site: 'kotobutsuen.com',
+     title: 'ことぶつえん',
+     reverse: true,
+     charset: 'utf-8',
+     description: '「ことぶつえん」は投稿型辞書アプリです。一緒にことばの新しい意味を考えてみない？',
+     separator: '|'
+     # icon: [
+       # { href: image_url('favicon.ico') },
+       # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+     # ],
+     # og: {
+       # site_name: 'サイト名',
+       # title: 'タイトル',
+       # description: 'description',
+       # type: 'website',
+       # url: request.original_url,
+       # image: image_url('ogp.png'),
+       # locale: 'ja_JP',
+     # },
+     # twitter: {
+       # card: 'summary',
+       # site: '@ツイッターのアカウント名',
+     # }
+   }
+ end
+
 end
